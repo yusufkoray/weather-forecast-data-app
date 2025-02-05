15c957d521cc156a81f9c56a8d273bb08a430731
ykorayc <bootcampyuzotuzalti@gmail.com>
2025-02-05 09:53:45 +0300

    Backed for getting data is done. Filtered the data according to Temperature and Sky.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import streamlit as st
-import pandas as pd
-
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place=st.text_input("Place:")
 days=st.slider("Forecast Days",min_value=1,max_value=5,help="Select the number of forecasted days")
 option=st.selectbox("Selacet data to view",("Temperature","Sky"))
-print(option)
 st.subheader(f"{option} for the next {days} days in {place}")
+
+data=get_data(days)
+
+d,t=get_data(days)
+figure=px.line(x=d,y=t,labels={'x':"Date",'y':"Temperature(C)"})
+st.plotly_chart(figure)
