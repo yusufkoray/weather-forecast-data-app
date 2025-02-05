489ec384cc2a17e27bf23a7805761539ef135149
ykorayc <bootcampyuzotuzalti@gmail.com>
2025-02-05 12:33:14 +0300

    Last commit

--- backend.py
+++ backend.py
@@ -3,24 +3,13 @@ import requests
 API_KEY="fbface564d5591fbf286daad81522d0a"
 
 
-def get_data(place,foreceast_day=None,kind=None):
+def get_data(place="Adana",foreceast_day=1):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response=requests.get(url)
     data=response.json()
-    filtered_data=data["list"]
     nr_values=8*foreceast_day  #24/3 = 8  8*5 = 40
     filtered_data=data["list"][:nr_values]
-    match kind:
-        case "Temperature":
-            filtered_data=[a["main"]["temp"] for a in filtered_data] # filtered_data'nin icerisinda a bir dictionary donuyor. Sonucta filtered datamiz dictionary.
-            #donen dictionary icerisindeki main key'inin icerisindeki "temp" key'ine erisiyoruz. Bunun icerisindeki temperature value'leri yeni bir lit olarak filtered_data
-            #icerisine aktariyoruz.
-            #yani aslinda her defasinda filtered_data[0]['main']['temp'] yapiyoruz. 0 bittikten sonra 1'e 1 'den sonra 2'ye geciyor.
-        case "Sky":
-            filtered_data=[b["weather"][0]["main"] for b in filtered_data] #Bu kisimda da [0] yazmamizin sebebi ['main'] ' in yani erismek istedigimiz
-        #degerin ['weather'] LISTESI icerisinde olmasindan kaynaklanir. Liste de sadece 1 eleman iceriyor.
-
     return filtered_data
 
 if __name__=="__main__":
-    print(get_data("Tokyo",foreceast_day=3,kind="Temperature"))
+    print(get_data("Tokyo",3))
--- main.py
+++ main.py
@@ -5,11 +5,41 @@ from backend import get_data
 st.title("Weather Forecast for the Next Days")
 place=st.text_input("Place:")
 days=st.slider("Forecast Days",min_value=1,max_value=5,help="Select the number of forecasted days")
-option=st.selectbox("Selacet data to view",("Temperature","Sky"))
+option=st.selectbox("Selact data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data=get_data(days)
 
-d,t=get_data(days)
-figure=px.line(x=d,y=t,labels={'x':"Date",'y':"Temperature(C)"})
-st.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperature_data = [a["main"]["temp"] for a in filtered_data]
+                                # filtered_data'nin icerisinda a bir dictionary donuyor. Sonucta filtered datamiz dictionary.
+                                # donen dictionary icerisindeki main key'inin icerisindeki "temp" key'ine erisiyoruz.
+                                #Bunun icerisindeki temperature value'leri yeni bir lit olarak filtered_data
+                                # icerisine aktariyoruz. Yani aslinda her defasinda filtered_data[0]['main']['temp'] yapiyoruz.
+                                #0 bittikten sonra 1'e 1 'den sonra 2'ye geciyor.
+            c_temperatures_data = [i-273 for i in temperature_data]
+
+            # Create a temperature plot.
+            days=[b["dt_txt"] for b in filtered_data]
+            figure=px.line(x=days,y=c_temperatures_data,labels={'x':"Date",'y':"Temperature(C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            filtered_data = [b["weather"][0]["main"] for b in filtered_data]
+                            # Bu kisimda da [0] yazmamizin sebebi ['main'] ' in yani erismek istedigimiz
+                            # degerin ['weather'] LISTESI icerisinde olmasindan kaynaklanir. Liste de sadece 1 eleman iceriyor.
+
+            #My Solution:
+            #filtered_data = ["images/" + str(c)[:1].lower() + str(c)[1:] + ".png" for c in filtered_data]
+
+            #Teacher Solution:
+            images={"Celar":"images/clear.png","Clouds":"images/clouds.png","Rain":"images/rain.png","Snow":"images/snow.png"}
+            image_paths=[images[condition] for condition in filtered_data]
+            # Create a sky image.
+            st.image(image_paths,width=115),
+    except KeyError:
+        st.info("This city is not accesable now. Please try another city :)")
+
+
